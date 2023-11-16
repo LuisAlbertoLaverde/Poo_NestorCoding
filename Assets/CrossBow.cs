7792ea07f2cc1126a96451cb3860e823d47311ba
Unumnio <unumnio@gmail.com>
2023-11-16 06:20:07 -0500

    starting project

---
+++ Assets/CrossBow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossBow : MonoBehaviour
+{
+    int damage;
+    string rarity;
+    void Shoot()
+    {
+        Debug.Log("pum");
+    }
+
+    void Recharge()
+    {
+        Debug.Log("recargando");
+    }
+}
